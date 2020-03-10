4297be942d637e960299a31a7dc98c4957849d6b
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-10 15:53:22 +0000

    GET request for user timeline

--- Routes/api-router.js
+++ Routes/api-router.js
@@ -1,5 +1,10 @@
 const apiRouter = require("express").Router();
 
-apiRouter.route("/").get(sendApiEndpoints);
+apiRouter.get("/", () => {
+  res.sendStatus(200);
+});
 
+apiRouter.get(
+  "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=pillarwallet&count=1"
+);
 module.exports = apiRouter;
