8b2b1dfc6be51831f7149702a6b0a067bcfc2441
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-10 17:16:41 +0000

    Added config for env

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
-const env = require("dotenv").config();
-
+const result = dotenv.config();
 const app = express();
 app.use(express.json());
 
+if (result.error) {
+  throw result.error;
+}
+
+console.log(result.parsed);
+
 app.use("/api", apiRouter);
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`listening on port 8000`);
+});
 
 module.exports = app;
