bbd6d5b46b59c1c5ca6db41a2cbbb3966c5888ff
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-11 17:01:10 +0000

    added credentials

--- sdk.js
+++ sdk.js
@@ -1,3 +1,16 @@
-const apiRouter = require("express").Router();
+const Twitter = require("twitter");
 
-// apiRouter.get("https://api.twitter.com/oauth/request_token");
+const client = new Twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+const params = { screen_name: "pillarwallet" };
+
+client.get("statuses/user_timeline", params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
