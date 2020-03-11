e3ae3cdcff27f1fe885a8507fafd1d0d0d0f25c1
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-11 17:22:15 +0000

    no changes

--- Routes/api-router.js
+++ Routes/api-router.js
@@ -4,8 +4,4 @@ apiRouter.get("/", () => {
   res.sendStatus(200);
 });
 
-apiRouter.get(
-  "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=pillarwallet&count=1"
-);
-
 module.exports = apiRouter;
