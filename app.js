c814542536076768aa4b2d7b619354da120c2147
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-11 17:31:53 +0000

    added auth to file

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ const express = require("express");
 const apiRouter = require("./routes/api-router");
 const result = dotenv.config();
 const app = express();
+const Twitter = require("twitter");
+
 app.use(express.json());
 
 if (result.error) {
   console.log(result.error);
 }
-
 console.log(result.parsed);
+
+const client = new Twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+const params = { screen_name: "pillarwallet" };
+
+client.get("statuses/user_timeline", params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+module.exports = client;
 
 app.use("/api", apiRouter);
 
