216f4e90421e307dfa8ff11394fabd446ae38051
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-12 18:27:52 +0000

    refactored data manipulation for tweets data

--- Controllers/twitterController.js
+++ Controllers/twitterController.js
@@ -2,7 +2,8 @@ const socialMedia = {};
 
 const fetchTweets = (error, tweets, response) => {
   if (!error) {
-    socialMedia.tweets = tweets;
+    socialMedia.tweets = tweets.filter(tweet => tweet.text);
+    console.log(socialMedia);
   } else if (error) {
     return response.status(error.status).send({ message: error.message });
   }
