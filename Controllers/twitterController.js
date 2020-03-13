d886bd52fa910b202ca90fa4d48a8ea5e4a0a869
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-13 15:55:52 +0000

    changed filter to map

--- Controllers/twitterController.js
+++ Controllers/twitterController.js
@@ -2,8 +2,9 @@ const socialMedia = {};
 
 const fetchTweets = (error, tweets, response) => {
   if (!error) {
-    socialMedia.tweets = tweets.filter(tweet => tweet.text);
-    console.log(socialMedia);
+    socialMedia.tweets = tweets.map(tweet => tweet.text);
+
+    console.log(socialMedia, "social media");
   } else if (error) {
     return response.status(error.status).send({ message: error.message });
   }
