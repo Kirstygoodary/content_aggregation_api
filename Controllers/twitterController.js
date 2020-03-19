bcf82039b121d7994a8f8fea00d316613020c9f6
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-19 17:03:09 +0000

    created tweet data objects

--- Controllers/twitterController.js
+++ Controllers/twitterController.js
@@ -2,9 +2,23 @@ const socialMedia = {};
 
 const fetchTweets = (error, tweets, response) => {
   if (!error) {
-    socialMedia.tweets = tweets.map(tweet => tweet.text);
+    //console.log(tweets[0].user.profile_image_url);
 
-    console.log(socialMedia, "social media");
+    const tweetArray = [];
+    tweets.forEach(tweet => {
+      const tweetObject = {};
+      tweetObject.name = tweet.user.name;
+      tweetObject.screenName = tweet.user.screen_name;
+      tweetObject.profilePicture = tweet.user.profile_image_url;
+      tweetObject.tweetText = tweet.text;
+      tweetObject.retweetUser = tweet.entities.user_mentions;
+      tweet.hashtags = tweet.entities.hashtags;
+      tweetObject.imageUrls = tweet.entities.urls;
+      tweetObject.retweets = tweet.retweet_count;
+      tweetObject.favouriteCount = tweet.favorite_count;
+
+      tweetArray.push(tweetObject);
+    });
   } else if (error) {
     return response.status(error.status).send({ message: error.message });
   }
