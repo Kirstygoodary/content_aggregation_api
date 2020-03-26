8a337cd263da25bcd339a8c092425ce41ced2046
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-03-26 14:22:06 +0000

    Refactored twitterController

--- Controllers/twitterController.js
+++ Controllers/twitterController.js
@@ -1,25 +1,13 @@
-const socialMedia = {};
+const { selectTweets } = require("../models/tweetsModel");
 
-const fetchTweets = (error, tweets, response) => {
-  if (!error) {
-    const tweetArray = [];
-    tweets.forEach(tweet => {
-      const tweetObject = {};
-      tweetObject.name = tweet.user.name;
-      tweetObject.screenName = tweet.user.screen_name;
-      tweetObject.profilePicture = tweet.user.profile_image_url;
-      tweetObject.tweetText = tweet.text;
-      tweetObject.retweetUser = tweet.entities.user_mentions;
-      tweet.hashtags = tweet.entities.hashtags;
-      tweetObject.imageUrls = tweet.entities.urls;
-      tweetObject.retweets = tweet.retweet_count;
-      tweetObject.favouriteCount = tweet.favorite_count;
-
-      tweetArray.push(tweetObject);
+const fetchTweets = (req, res, next) => {
+  selectTweets()
+    .then(response => {
+      res.status(200).send({ response });
+    })
+    .catch(err => {
+      next(err);
     });
-  } else if (error) {
-    return response.status(error.status).send({ message: error.message });
-  }
 };
 
 module.exports = { fetchTweets };
